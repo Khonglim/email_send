2e110174a2c901a6240fbae1183fbb1a21e34c87
jame <narathorn182539@gmail.com>
2018-11-15 09:28:39 +0700

    jame

--- config/app.php
+++ config/app.php
@@ -147,7 +147,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Anhskohbo\NoCaptcha\NoCaptchaServiceProvider::class,
-
+      
         /*
          * Package Service Providers...
          */
@@ -210,6 +210,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'NoCaptcha' => Anhskohbo\NoCaptcha\Facades\NoCaptcha::class,
+     
 
     ],
 
--- resources/views/emails/send.blade.php
+++ resources/views/emails/send.blade.php
@@ -1,119 +1,110 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet"  href="https://fonts.googleapis.com/css?family=Prompt">
+    <link href="{{ asset('css_nggtp/bootstrap.css') }}"   rel='stylesheet' type='text/css' />
+    <link href="{{ asset('css_nggtp/style.css') }}"   rel='stylesheet' type='text/css' />
+    <link href="{{ asset('css_nggtp/style2.css') }}"   rel='stylesheet' type='text/css' />
+    <link href="{{ asset('css_nggtp/dark.css') }}"   rel='stylesheet' type='text/css' />
 
-@section('content')
-<script src='https://www.google.com/recaptcha/api.js'></script>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <link href="{{ asset('css_nggtp/rolex.css') }}"   rel='stylesheet' type='text/css' />
+    <link href="{{ asset('css_nggtp/font-icons.css') }}"   rel='stylesheet' type='text/css' />
+    <link href="{{ asset('css_nggtp/animate.css') }}"   rel='stylesheet' type='text/css' />
+    <link href="{{ asset('css_nggtp/magnific-popup.css') }}"   rel='stylesheet' type='text/css' />
+    <link href="{{ asset('css_nggtp/responsive.css') }}"   rel='stylesheet' type='text/css' />
+    <link href="{{ asset('css_nggtp/colors.php') }}"   rel='stylesheet' type='text/css' />
 
-                <div class="card-body">
-                <form method="POST" action="{{url('email')}}" aria-label="{{ __('Register') }}"  id="inviter">
-                        @csrf
-                       
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Firstname') }}</label>
+    <link href="{{ asset('css_nggtp/datepicker.css') }}"   rel='stylesheet' type='text/css' />
+    <link href="{{ asset('css_nggtp/timepicker.css') }}"   rel='stylesheet' type='text/css' />
+    <link href="{{ asset('css_nggtp/daterangepicker.css') }}"   rel='stylesheet' type='text/css' />
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="fristname" value="{{ old('name') }}" required autofocus>
+    <script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script>   
+</head>
+<body>
+    <img src="https://www.nggtimepieces.com/css/desktop/demos/rolex/images/calender_2.png" width="120" height="120">
+        <center><h2>BOOK AN APPOINTMENT</h2></center>   
+    <div class="row">
+        <div class="col-md-6"> 
+            <div class="form-group ">
+                <label class="control-label">FIRST NAME<small class="text-danger">*</small></label>
+                <input type="text" name="first_name" id="first_name" class="form-control">
+          
+                <b  class="text-danger" id="first_nameError"></b>
+                </div>  
+            </div>
+        <div class="col-md-6"> 
+            <div class="form-group ">
+                <label class="control-label">LAST NAME<small class="text-danger">*</small></label>
+                <input type="text" name="last_name" id="last_name" class="form-control">
 
-                 
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="lastname" class="col-md-4 col-form-label text-md-right">{{ __('Lastname') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="lastname" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="lastname" value="{{ old('name') }}" required autofocus>
-
-                        
-                            </div>
-                        </div>
-
-                        
-                        <div class="form-group row">
-                            <label for="lastname" class="col-md-4 col-form-label text-md-right">{{ __('Phone') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="phone" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="phone" value="{{ old('name') }}" required autofocus>
-
-                        
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                              
-                            </div>
-                        </div>
-                                 
-                        <div class="form-group row">
-                            <label for="lastname" class="col-md-4 col-form-label text-md-right">{{ __('Store') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="Store" type="text" class="form-control" name="store" value="{{ old('name') }}" required autofocus>
-
-                        
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="message" class="col-md-4 col-form-label text-md-right">{{ __('message') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="message" type="text" class="form-control" name="message" required>
-
-                            
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                                <label for="message" class="col-md-4 col-form-label text-md-right">{{ __('date') }}</label>
-    
-                                <div class="col-md-6">
-                                    <input id="message" type="date" class="form-control" name="date" required>
-    
-                                
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                    <label for="message" class="col-md-4 col-form-label text-md-right"></label>
-                                    {!! NoCaptcha::display() !!}
-
-                                    @if ($errors->has('g-recaptcha-response'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
-                                    </span>
-                                @endif
-                                    </div>
-                                
-                               
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <span id="sendingmail">  <button type="submit" class="btn btn-primary " >
-                                    {{ __('Register') }}
-                                </button>
-                                </span>
-                            </div>
-                        </div>
-
-  
-                    </form>
-                </div>
+                <b  class="text-danger" id="last_nameError"></b>
+                </div>  
             </div>
         </div>
-    </div>
-</div>
-<script>
+    <div class="row">
+        <div class="col-md-6"> 
+            <div class="form-group ">
+                <label class="control-label">PHONE NUMBER<small class="text-danger">*</small></label>
+                <input type="text" name="phone" id="phone" class="form-control">
 
-$("#inviter").submit(function(){    
-    
-     $("#sendingmail").html("ระบบกำลังทำการส่งอีเมล์โปรดรอสักครู่.....");    
-     
-      return true; });
+               
+                <b  class="text-danger" id="phoneError"></b>
+                </div>  
+            </div>
+        <div class="col-md-6"> 
+            <div class="form-group ">
+                <label class="control-label">EMAIL<small class="text-danger">*</small></label>
+                <input type="email" name="email" id="email" class="form-control">
+              
+                <b  class="text-danger" id="emailError"></b>
+                </div>  
+            </div>
+        </div>
+    <div class="row">
+        <div class="col-md-6"> 
+            <div class="form-group ">
+                <label class="control-label">STORE<small class="text-danger">*</small></label>
+                <select name="stroe" id="stroe" class="form-control">
+                    <option value="NGG Timepieces Head Office">NGG Timepieces Head Office</option>
+                    <option value="Central World">Central World</option>
+                    <option value="Central Plaza Ladprao">Central Plaza Ladprao</option>
+                    <option value="Central Plaza WestGate">Central Plaza WestGate</option>
+                    <option value="Central Plaza Udonthani">Central Plaza Udonthani</option>
+                   
+                </select>
+               
+                <b  class="text-danger" id="stroeError"></b>
+                </div>  
+            </div>
+        <div class="col-md-6"> 
+            <div class="form-group ">
+                <label class="control-label">DATE<small class="text-danger">*</small></label>
+                <input type="text" name="date" id="date" class="form-control">
+                
+                <b  class="text-danger" id="dateError"></b>
+                </div>  
+            </div>
+        </div>
+    <div class="row">
+        <div class="col-md-12"> 
+            <div class="form-group ">
+                <label class="control-label">MESSAGE<small class="text-danger">*</small></label>
+                <input type="text" name="message" id="message" class="form-control">
+                
+                <b  class="text-danger" id="messageError"></b>
+                </div>  
+            </div>
+        </div>
 
-</script>
-@endsection
+        <div class="form-group">
+            <br>
+                    <center><button type="button" class="btn btn-default">RESERVE</button></center>
+            </div>
+        
+</body>
+</html>
